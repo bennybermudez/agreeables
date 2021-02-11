a5b633e8d8de96f43dd42866a57d54503dafdade
Benny Bermudez <bennybermudez@Bennys-MacBook-Air.local>
2021-02-10 21:05:34 -0800

    First Init

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "info@theagreeables.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+header("Location: http://theagreeables.com/thank-you");  
+?>
